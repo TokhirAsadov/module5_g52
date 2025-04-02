5bf109eb26a38b714b51d3244d83ca225f12aa11
Takhir <guvalakat1603@gmail.com>
2025-04-02 18:11:15 +0500

    Mailing

--- src/main/java/uz/javatuz/mailing/MailingExample.java
+++ src/main/java/uz/javatuz/mailing/MailingExample.java
@@ -1,12 +1,41 @@
 package uz.javatuz.mailing;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
 public class MailingExample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MessagingException {
 
         Properties properties = new Properties();
-        properties.put("")
+        properties.put("mail.smtp.host","smtp.gmail.com");
+        properties.put("mail.smtp.port","465");
+        properties.put("mail.smtp.ssl.enable","true");
+        properties.put("mail.smtp.auth","true");
+
+        String username = "guvalakat1603@gmail.com";
+        String password = "eysbnpqdqjsjqqlv";
+
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username,password);
+            }
+        });
+
+        Message message = new MimeMessage(session);
+        message.setSubject("Yaxshi xabar");
+//        message.setText("Xabar keldi G52 dan...");
+        message.setContent("<h1 style=\"color: red;\">Html message qabul qilindi</h1>","text/html");
+
+        message.setFrom(new InternetAddress(username));
+        message.setRecipient(Message.RecipientType.TO,new InternetAddress("devatpdp@gmail.com"));
+
+        Transport.send(message);
+        System.out.println("xabar junatildi.............");
+
+
 
     }
 }
