cfcfb42f728de0ad3a5e9bdb809de1d085885c21
Takhir <guvalakat1603@gmail.com>
2025-04-02 16:58:54 +0500

    Mailing first lesson of module 5

---
+++ src/main/java/uz/javatuz/mailing/MailingExample.java
@@ -0,0 +1,12 @@
+package uz.javatuz.mailing;
+
+import java.util.Properties;
+
+public class MailingExample {
+    public static void main(String[] args) {
+
+        Properties properties = new Properties();
+        properties.put("")
+
+    }
+}
